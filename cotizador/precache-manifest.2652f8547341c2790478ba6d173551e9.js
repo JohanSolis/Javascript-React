9f3a8bd3153651ecf7bff4c0affd5da6f156c545
Johan Solís <66803996+JohanSolis@users.noreply.github.com>
2021-02-21 17:57:59 -0600

    Add files via upload

---
+++ cotizador/precache-manifest.2652f8547341c2790478ba6d173551e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510f40437955ff77801aac8415b085ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b98f127b8a91b83d374",
+    "url": "/static/css/main.6919f943.chunk.css"
+  },
+  {
+    "revision": "d40dd198fc7a573faad4",
+    "url": "/static/js/2.608add4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.608add4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b98f127b8a91b83d374",
+    "url": "/static/js/main.ad42174d.chunk.js"
+  },
+  {
+    "revision": "67e45de583dadb924782",
+    "url": "/static/js/runtime-main.620d9de5.js"
+  }
+]);
